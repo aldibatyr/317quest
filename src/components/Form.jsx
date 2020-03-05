30c5dad7b87e94988ae51ecee6ddc948ccaef55d
Aldiyar Batyrbekov <aldibatyrbekov@gmail.com>
2020-03-04 16:36:19 -0800

    changed form behavior to send message

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Col, Row } from 'react-bootstrap';
 import styled from 'styled-components';
 
-const Form = styled.form `
+const Form = styled.form`
     width: 100%;
     display: flex;
     flex-wrap: wrap;
@@ -42,29 +42,65 @@ export const Button = styled.button`
     padding: 18px 32px;
 `
 
+function encode(data) {
+    return Object.keys(data)
+        .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&')
+}
 
 const FormComponent = () => {
+
+    const [state, setState] = useState({});
+
+    const handleChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value })
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const form = e.target
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: encode({
+                'form-name': form.getAttribute('name'),
+                ...state,
+            }),
+        })
+            .then(() => alert('Sent'))
+            .catch((error) => alert(error))
+    }
+
     return (
-        <Form name="contact" method="POST" data-netlify="true">
-            <Input width="full">
-                <input type="text" name="name" id="name" placeholder="Your Name" />
-            </Input>
-            <Input>
-                <input type="email" name="email" id="email" placeholder="Your Email"/>
-            </Input>
-            <Input>
-                <input type="tel" name="phone" id="phone" placeholder="Your Phone"/>
-            </Input> 
-            <Input width="full">
-                <textarea name="message" id="message" rows="6" spellCheck="true" placeholder="Your Message">
-                </textarea>
-            </Input>
-            <Controls>
-                <Button>
-                    <span style={{fontWeight: '700', color: 'white', lineHeight: '0'}}>SEND A MESSAGE</span>
-                </Button>
-            </Controls>
-        </Form>
+        <>
+            <Form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={e => handleSubmit(e)}>
+                <input type="hidden" name="form-name" value="contact" />
+                <p hidden>
+                    <label>
+                        Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+                    </label>
+                </p>
+                <Input width="full">
+                    <input type="text" name="name" id="name" placeholder="Your Name" onChange={e => handleChange(e)} />
+                </Input>
+                <Input>
+                    <input type="email" name="email" id="email" placeholder="Your Email" onChange={e => handleChange(e)} />
+                </Input>
+                <Input>
+                    <input type="tel" name="phone" id="phone" placeholder="Your Phone" onChange={e => handleChange(e)} />
+                </Input>
+                <Input width="full">
+                    <textarea name="message" id="message" rows="6" spellCheck="true" placeholder="Your Message" onChange={e => handleChange(e)}>
+                    </textarea>
+                </Input>
+                <Controls>
+                    <Button>
+                        <span style={{ fontWeight: '700', color: 'white', lineHeight: '0' }}>SEND A MESSAGE</span>
+                    </Button>
+                </Controls>
+            </Form>
+        </>
     )
 }
 
