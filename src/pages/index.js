825d8135dbdebb74c2a6b9f04f2bde2980bb1142
aldibatyr@gmail.com <aldibatyrbekov@gmail.com>
2020-12-02 20:57:11 +0600

    added confetti animations for index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,23 +2,34 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Hero from '../components/Hero';
-import About from "../components/About";
-import VideoPreview from "../components/VideoPreview";
-import Overview from "../components/Overview";
-import Facts from "../components/Facts";
-import SignUp from "../components/SignUp";
+import Hero from "../components/Hero"
+import About from "../components/About"
+import VideoPreview from "../components/VideoPreview"
+import Overview from "../components/Overview"
+import Facts from "../components/Facts"
+import SignUp from "../components/SignUp"
+import Confetti from "react-confetti"
+import { useWindowSize } from "react-use"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Hero/>
-    <About/>
-    <VideoPreview/>
-    <Overview/>
-    <Facts/>
-    <SignUp/>
-  </Layout>
-)
+const IndexPage = () => {
+  const { width, height } = useWindowSize()
+  return (
+    <Layout>
+      <Confetti
+        tweenDuration={5000}
+        width={width}
+        height={height}
+        recycle={false}
+      />
+      <SEO title="Home" />
+      <Hero />
+      <About />
+      <VideoPreview />
+      <Overview />
+      <Facts />
+      <SignUp />
+    </Layout>
+  )
+}
 
 export default IndexPage
